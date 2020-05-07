59f79b32a54d00c452e64678c75c62c52a3c48e7
tamubun <sutebubon@gmail.com>
2020-05-07 22:43:33 +0900

    Windowsで簡易サーバーを立てられるようにserver.py, server.exeを追加
    server.exeは、pyinstallerを使って server.pyから作った物

---
+++ server.py
@@ -0,0 +1,25 @@
+from http.server import *
+
+def test(HandlerClass=BaseHTTPRequestHandler,
+         ServerClass=HTTPServer, protocol="HTTP/1.0", port=8000, bind=""):
+    """Test the HTTP request handler class.
+
+    This runs an HTTP server on port 8000 (or the port argument).
+
+    """
+    server_address = (bind, port)
+
+    HandlerClass.protocol_version = protocol
+    with ServerClass(server_address, HandlerClass) as httpd:
+        sa = httpd.socket.getsockname()
+        serve_message = "Serving HTTP on {host} port {port} (http://{host}:{port}/) ..."
+        print(serve_message.format(host=sa[0], port=sa[1]))
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt received, exiting.")
+            sys.exit(0)
+
+if __name__ == '__main__':
+    handler_class = SimpleHTTPRequestHandler
+    test(HandlerClass=handler_class, port=8000)
